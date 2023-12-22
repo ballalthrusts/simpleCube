7cfd776f2b8ce58e2dea4c8cade80fc962aeaf32
Aniruddh Ballal <131568375+ballalthrusts@users.noreply.github.com>
2023-12-22 15:28:52 +0530

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include "Cube.h"
 #include<iostream>
 
+//works only when the header class "Cube.h" is NOT wrapped by a namespace. 
+
 int main() {
     Cube c;
 
@@ -8,5 +10,5 @@ int main() {
     double volume = c.getVolume();
     std::cout << "Volume: " << volume <<std::endl;
     
-    return 0;
+    return 17;
 }
